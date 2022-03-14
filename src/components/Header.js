bb65f5bb46bc3d0c5ba4595a5c9176b414c43047
albert chen <k60513albert@yahoo.com.tw>
2022-03-14 18:33:04 +0800

    (接收端)props.title

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-const Header = ({ title }) => {
+const Header = (props) => {
   return (
       <View style={styles.headerStyle}>
-        <Text style={styles.textStyle}>{title}</Text>
+        <Text style={styles.textStyle}>{props.title}</Text>
       </View>
   );
 };
