2f34251bdf0269d5213bd2b66ea094e9e299a615
albert chen <k60513albert@yahoo.com.tw>
2022-03-24 02:37:11 +0800

    hw1-4

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import  NewDetailScreen  from './src/screens/NewDetail';
 import HomeScreen from './src/screens/Home';
 //import Navigator from './route/homeStack'
 
 
 const Stack = createNativeStackNavigator();
+//const Tab = createBottomTabNavigator();
 
 const App = () => {
   return (
